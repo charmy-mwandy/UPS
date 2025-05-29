c218ff01c65fda022322f1b1bf1a5b16455b8b8a
Charmy developer <charmainemwandiringa@gmail.com>
2025-05-29 18:05:33 +0200

    add bottom tab navigation

--- navigator/Rootnavigator.tsx
+++ navigator/Rootnavigator.tsx
@@ -1,11 +1,12 @@
 import {  Text, View } from 'react-native'
 import React from 'react'
-import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Tabnavigator from './Tabnavigator';
 
 export type RootStackParamList = {
     Main: undefined;
+    Mymodel:  {useId: string; name: string};
+    Order: {order: any;}
 }
 
 const RootStack = createNativeStackNavigator();
--- navigator/Tabnavigator.tsx
+++ navigator/Tabnavigator.tsx
@@ -1,11 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CustomerScreen from '../screens/CustomerScreen';
+import OrderScreen from '../screens/OrderScreen';
+import { useNavigation } from '@react-navigation/native';
+
+export type TabStackParamList = {
+    Customers: undefined;
+    Orders: undefined;
+}
+
+const Tab = createBottomTabNavigator();
 
 const Tabnavigator = () => {
+const navigation = useNavigation();
+
+useLayoutEffect( () => {
+    navigation.setOptions({
+        headerShown: false,
+    });
+}, [])
+
+
+
   return (
-    <View>
-      <Text>Tabnavigator</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name='Customers' component={CustomerScreen} />
+      <Tab.Screen name='Orders' component={OrderScreen} />
+    </Tab.Navigator>
   )
 }
 
